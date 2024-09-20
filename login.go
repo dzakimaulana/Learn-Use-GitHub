c123b69b87494b6d324a56680dfb100c2084aab6
dzakimaulana <dzakimaulanapati@gmail.com>
2024-09-20 20:50:49 +0700

    Add hash function for security reason

--- login.go
+++ login.go
@@ -3,6 +3,8 @@ package main
 import (
 	"errors"
 	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Account struct {
@@ -12,14 +14,38 @@ type Account struct {
 
 var accounts []Account
 
+func hashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
+}
+
+func checkPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
 func register(username string, password string) error {
 	if username == "" || password == "" {
 		return errors.New("username or password cannot be empty")
 	}
 
+	for _, account := range accounts {
+		if account.Username == username {
+			return errors.New("username already exists")
+		}
+	}
+
+	hashedPassword, err := hashPassword(password)
+	if err != nil {
+		return err
+	}
+
 	accounts = append(accounts, Account{
 		Username: username,
-		Password: password,
+		Password: hashedPassword,
 	})
 
 	fmt.Printf("Successfully register %s", username)
@@ -33,11 +59,12 @@ func login(username string, password string) error {
 
 	for _, account := range accounts {
 		if account.Username == username {
-			if account.Password == password {
+			match := checkPasswordHash(password, account.Password)
+			if match {
 				return nil
 			}
+			return errors.New("username or password incorrect")
 		}
-		return errors.New("username or password incorrect")
 	}
 	return errors.New("username or password incorrect")
 }
