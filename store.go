b8d41b74e4ae3581618085d3cb4c1272964033b0
dzakimaulana <dzakimaulanapati@gmail.com>
2024-09-21 12:06:47 +0700

    Add chitato

--- store.go
+++ store.go
@@ -11,4 +11,5 @@ var stuff = []Items{
 	{Name: "Aqua", Type: "Dring", Price: 5000, Stock: 10},
 	{Name: "Mamoka", Type: "Food", Price: 4000, Stock: 20},
 	{Name: "Le Minerale", Type: "Drink", Price: 4000, Stock: 50},
+	{Name: "Chitato", Type: "Food", Price: 8000, Stock: 15},
 }
